397adc8f06f8d230fcac43c6e93599eca3e99cb1
jhs512 <jangka512@gmail.com>
2024-12-29 05:45:37 +0900

    work

---
+++ backend/src/main/kotlin/com/ll/backend/domain/home/home/controller/HomeController.kt
@@ -0,0 +1,21 @@
+package com.ll.backend.domain.home.home.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.net.InetAddress
+
+
+@Controller
+class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    fun showMain(): String {
+        val localHost = InetAddress.getLocalHost()
+
+        // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
+        val hostname = localHost.hostName
+
+        return "홈, hostname : $hostname"
+    }
+}
