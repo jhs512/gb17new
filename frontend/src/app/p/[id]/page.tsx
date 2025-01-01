0aa87e8351dff1682196a6c84d9386e43e6b183d
jhs512 <jangka512@gmail.com>
2025-01-01 23:52:25 +0900

    work

--- frontend/src/app/p/[id]/page.tsx
+++ frontend/src/app/p/[id]/page.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-
 import client from "@/lib/backend/client";
 import { cookies } from "next/headers";
 import ClientPage from "./ClientPage";
 
-// meta
-export const metadata: Metadata = {
-  title: "Post",
-  description: "",
-};
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+async function getPost(id: string) {
   const res = await client.GET("/api/v1/posts/{id}", {
     params: {
       path: {
@@ -24,25 +16,50 @@ export default async function Page({ params }: { params: { id: string } }) {
   });
 
   if (res.error) {
-    return <div>{res.error.msg}</div>;
+    return null;
   }
 
-  const post = res.data!!;
+  return res.data!!;
+}
 
-  function processMarkdown(input: string) {
-    // 1. $$??? 내용 $$ 패턴 제거
-    input = input.replace(/\$\$[\s\S]*?\$\$/g, "");
+function processMarkdown(input: string) {
+  // 1. $$??? 내용 $$ 패턴 제거
+  input = input.replace(/\$\$[\s\S]*?\$\$/g, "");
 
-    // 2. ``` 패턴으로 감싸진 코드 블록 제거
-    input = input.replace(/```[\s\S]*?```/g, "");
+  // 2. ``` 패턴으로 감싸진 코드 블록 제거
+  input = input.replace(/```[\s\S]*?```/g, "");
 
-    // 3. 특수 기호 #와 - 만 남기기
-    const result = input.match(/[#-]/g) || []; // #와 -만 추출
-    return result.join(""); // 배열을 문자열로 변환
+  // 3. 특수 기호 #와 - 만 남기기
+  const result = input.match(/[#-]/g) || []; // #와 -만 추출
+  return result.join(""); // 배열을 문자열로 변환
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const post = await getPost(params.id);
+
+  if (!post) {
+    return {
+      title: "Error",
+      description: "Post not found",
+    };
   }
 
-  metadata.title = post.title;
-  metadata.description = processMarkdown(post.content);
+  return {
+    title: post.title,
+    description: processMarkdown(post.content),
+  };
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const post = await getPost(params.id);
+
+  if (!post) {
+    return <div>게시글을 찾을 수 없습니다.</div>;
+  }
 
   return <ClientPage post={post} />;
 }
