94c855688512cc097b08bc44f28710a37b392236
jhs512 <jangka512@gmail.com>
2025-01-27 11:03:48 +0900

    work

--- backend/src/main/kotlin/com/ll/backend/domain/youtube/query/playlistEntry/controller/GotoYoutubeByPlaylistEntryController.kt
+++ backend/src/main/kotlin/com/ll/backend/domain/youtube/query/playlistEntry/controller/GotoYoutubeByPlaylistEntryController.kt
@@ -1,15 +1,11 @@
 package com.ll.backend.domain.youtube.query.playlistEntry.controller
 
 import com.ll.backend.domain.youtube.query.playlistEntry.service.YoutubeApiPlaylistEntryService
-import com.ll.backend.standard.extensions.getOrThrow
-import com.ll.backend.standard.extensions.handleExceptions
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("/goto/youtube/{playlistCode}")
@@ -17,38 +13,58 @@ class GotoYoutubeByPlaylistEntryController(
     private val fqYoutubeApiPlaylistEntryService: YoutubeApiPlaylistEntryService
 ) {
     @GetMapping("/{number}")
+    @ResponseBody
     fun gotoYoutubeByNumber(
         @PathVariable playlistCode: String,
         @PathVariable number: Int
-    ): ResponseEntity<String> = handleExceptions {
-        val playlistEntryDtos = fqYoutubeApiPlaylistEntryService
-            .findByPlaylistCode(playlistCode)
-            .getOrThrow()
-
-        val playlistEntryDtosIndex = if (number < 0) {
-            playlistEntryDtos.size + number
-        } else {
-            number - 1
-        }
-
-        if (playlistEntryDtosIndex < 0 || playlistEntryDtosIndex >= playlistEntryDtos.size) {
-            val url = "https://www.youtube.com/playlist?list=$playlistCode"
-
-            // ResponseEntity 생성
-            return ResponseEntity.status(HttpStatus.FOUND) // 302 상태 코드
-                .header(HttpHeaders.LOCATION, url) // 리다이렉션 URL 설정
-                .header(HttpHeaders.CACHE_CONTROL, "max-age=1800") // 30분 동안 캐시
-                .build()
-        }
-
-        val playlistEntryDto = playlistEntryDtos[playlistEntryDtosIndex]
-
-        val url = "https://youtu.be/${playlistEntryDto.code}"
-
-        // ResponseEntity 생성
-        return ResponseEntity.status(HttpStatus.FOUND) // 302 상태 코드
-            .header(HttpHeaders.LOCATION, url) // 리다이렉션 URL 설정
-            .header(HttpHeaders.CACHE_CONTROL, "max-age=1800") // 2분 동안 캐시
-            .build()
+    ): String {
+        return """
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="3;url=https://www.youtube.com/playlist?list=$playlistCode">
+                </head>
+                <body>
+                    <h1>Redirecting...</h1>
+                    <h2><span style="color:red;">${number}번</span>째 영상을 확인해주세요.</h2>
+                    <p>Redirecting to <a href="https://www.youtube.com/playlist?list=$playlistCode">https://www.youtube.com/playlist?list=$playlistCode</a></p>
+                </body>
+            </html>
+        """.trimIndent()
     }
+
+//    @GetMapping("/{number}")
+//    fun gotoYoutubeByNumber(
+//        @PathVariable playlistCode: String,
+//        @PathVariable number: Int
+//    ): ResponseEntity<String> = handleExceptions {
+//        val playlistEntryDtos = fqYoutubeApiPlaylistEntryService
+//            .findByPlaylistCode(playlistCode)
+//            .getOrThrow()
+//
+//        val playlistEntryDtosIndex = if (number < 0) {
+//            playlistEntryDtos.size + number
+//        } else {
+//            number - 1
+//        }
+//
+//        if (playlistEntryDtosIndex < 0 || playlistEntryDtosIndex >= playlistEntryDtos.size) {
+//            val url = "https://www.youtube.com/playlist?list=$playlistCode"
+//
+//            // ResponseEntity 생성
+//            return ResponseEntity.status(HttpStatus.FOUND) // 302 상태 코드
+//                .header(HttpHeaders.LOCATION, url) // 리다이렉션 URL 설정
+//                .header(HttpHeaders.CACHE_CONTROL, "max-age=1800") // 30분 동안 캐시
+//                .build()
+//        }
+//
+//        val playlistEntryDto = playlistEntryDtos[playlistEntryDtosIndex]
+//
+//        val url = "https://youtu.be/${playlistEntryDto.code}"
+//
+//        // ResponseEntity 생성
+//        return ResponseEntity.status(HttpStatus.FOUND) // 302 상태 코드
+//            .header(HttpHeaders.LOCATION, url) // 리다이렉션 URL 설정
+//            .header(HttpHeaders.CACHE_CONTROL, "max-age=1800") // 2분 동안 캐시
+//            .build()
+//    }
 }
